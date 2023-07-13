09c60d443aa43bc0271c485da6786e99dbce7441
Yuxiang Skyler Han <105407516+pineandbamboo@users.noreply.github.com>
2023-07-13 12:18:53 -0400

    Delete src/main directory

--- src/main/java/com/example/mgc_library/entity/response/Book.java
+++
@@ -1,9 +0,0 @@
-package com.example.mgc_library.entity.response;
-
-public class Book {
-    private String name;
-    private String author;
-    private String publishDate;
-    private String ISBN;
-    private double price;
-}
