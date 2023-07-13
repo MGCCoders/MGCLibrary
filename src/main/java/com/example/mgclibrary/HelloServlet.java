3afec4f0e95c1c0506f78780744d1dc442e0ef65
Yuxiang Skyler Han <105407516+pineandbamboo@users.noreply.github.com>
2023-07-13 12:41:05 -0400

    updated the dependencies added servlet for get/post for books

--- src/main/java/com/example/mgclibrary/HelloServlet.java
+++ src/main/java/com/example/mgclibrary/HelloServlet.java
@@ -1,27 +1,48 @@
 package com.example.mgclibrary;
 
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
+
 import java.io.*;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+@WebServlet(name = "helloServlet", value = {"/book"})
 public class HelloServlet extends HttpServlet {
-    private String message;
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Read game information from request body
+        JSONObject jsonRequest = new JSONObject(IOUtils.toString(request.getReader()));
+        String name = jsonRequest.getString("name");
+        String author = jsonRequest.getString("author");
+        String publishDate = jsonRequest.getString("publish_date");
+        String ISBN = jsonRequest.getString("ISBN");
+        float price = jsonRequest.getFloat("price");
+        // Print game information to IDE console
+        System.out.println("Name is: " + name);
+        System.out.println("Author is: " + author);
+        System.out.println("Publish date is: " + publishDate);
+        System.out.println("ISBN is: " + ISBN);
+        System.out.println("Price is: " + price);
+        // Return status = ok as response body to the client
+        response.setContentType("application/json");
+        JSONObject jsonResponse = new JSONObject();
+        jsonResponse.put("status", "ok");
+        response.getWriter().print(jsonResponse);
 
-    public void init() {
-        message = "Hello World!";
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        JSONObject book = new JSONObject();
+        book.put("name", "David and Goliath");
+        book.put("author", "John Smith");
+        book.put("publish_date", "Jan 11, 1999");
+        book.put("ISBN", "1234567890");
+        book.put("price", 49.99);
 
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
-    }
+        // Write game information to response body
+        response.getWriter().print(book);
 
-    public void destroy() {
     }
 }
